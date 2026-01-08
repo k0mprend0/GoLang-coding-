7fbb0d43b7f2d8fca724b5d83012620dd162aed0
k0mprend0 <156547649+k0mprend0@users.noreply.github.com>
2026-01-09 01:55:22 +0300

    Calculator
    There is no introducing to fyne.io code. In this update Ive made my first calculacor (app)

--- test web app/main.go
+++ test web app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
@@ -10,23 +11,39 @@ import (
 
 func main() {
 	myApp := app.New()
-	window := myApp.NewWindow("Hello World")
+	window := myApp.NewWindow("Mini calculator")
 
-	label := widget.NewLabel("Hello everyone!")
+	label1 := widget.NewLabel("Enter ur 1st number")
+	entry1 := widget.NewEntry()
 
-	//input field
-	entry := widget.NewEntry()
+	label2 := widget.NewLabel("Enter ur 2nd number")
+	entry2 := widget.NewEntry()
 
-	//creating buttons
-	button := widget.NewButton("Press me!", func() {
-		data := entry.Text // получение данных из input fields
-		fmt.Println(data)
+	answer := widget.NewLabel("")
+
+	button := widget.NewButton("Start calculating", func() {
+		n1, err1 := strconv.ParseFloat(entry1.Text, 64) // конвертация из str в float (64 - колво бит)
+		n2, err2 := strconv.ParseFloat(entry2.Text, 64)
+
+		if err1 != nil || err2 != nil {
+			answer.SetText("Input error!")
+		} else {
+			sum := n1 + n2
+			sub := n1 - n2
+			mult := n1 * n2
+			div := n1 / n2
+
+			answer.SetText(fmt.Sprintf("Сумма: %f\nРазность: %f\nПроизведение: %f\nЧастное: %f", sum, sub, mult, div))
+		}
 	})
 
 	window.SetContent(container.NewVBox(
-		label,
-		entry,
+		label1,
+		entry1,
+		label2,
+		entry2,
 		button,
+		answer,
 	))
 
 	//window.SetContent(widget.NewLabel("Hello World!"))
