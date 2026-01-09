194244a9d83b45d96d47dce3aaaf8fab9dee1812
k0mprend0 <156547649+k0mprend0@users.noreply.github.com>
2026-01-09 12:59:05 +0300

    Working with colors
    In this file im trying to work with colors in Golang .
    
    IMPORTANT!! var A in golang from 0 to 255!! not from 0 to 1

--- test web app/main.go
+++ test web app/main.go
@@ -1,54 +1,22 @@
 package main
 
 import (
+	"image/color"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/canvas"
 )
 
 func main() {
 	myApp := app.New()
-	window := myApp.NewWindow("Mini calculator")
-	window.Resize(fyne.NewSize(300, 500))
+	window := myApp.NewWindow("My app")
+	window.Resize(fyne.NewSize(900, 600))
 
-	title := widget.NewLabel("Оформление заказа")
+	color_for := color.NRGBA{R: 136, G: 0, B: 147, A: 255}
+	label := canvas.NewText("Color", color_for)
 
-	name_label := widget.NewLabel("Ваше имя: ")
-	name := widget.NewEntry()
-
-	food_label := widget.NewLabel("Выберите еду для заказа: ")
-
-	food := widget.NewCheckGroup([]string{"Pizza", "Cake", "Nuggets", "Burgers", "Soda"}, func(s []string) {})
-
-	male_label := widget.NewLabel("Ваш пол: ")
-	male := widget.NewRadioGroup([]string{"Муж", "Жен", "Оно"}, func(s string) {})
-
-	result := widget.NewLabel("")
-
-	button := widget.NewButton("Order it!", func() {
-		username := name.Text
-		food_arr := food.Selected
-		male_val := male.Selected
-
-		result.SetText(username + "\n" + male_val + "\n")
-
-		for _, i := range food_arr {
-			result.SetText(result.Text + i + "\n")
-		}
-	})
-
-	window.SetContent(container.NewVBox(
-		title,
-		name_label,
-		name,
-		food_label,
-		food,
-		male_label,
-		male,
-		button,
-		result,
-	))
+	window.SetContent(label)
 
 	//window.SetContent(widget.NewLabel("Hello World!"))
 	window.ShowAndRun()
