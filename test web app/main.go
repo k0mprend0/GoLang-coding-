d90691110fc5d9dc3af6435e4fb426aef2e5b539
k0mprend0 <156547649+k0mprend0@users.noreply.github.com>
2026-01-09 17:47:21 +0300

    Accordions
    Just playing with accordions

--- test web app/main.go
+++ test web app/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
@@ -15,82 +13,96 @@ func main() {
 	window.Resize(fyne.NewSize(400, 400))
 	myApp.Settings().SetTheme(theme.DarkTheme())
 
-	file_item1 := fyne.NewMenuItem("New file", func() {
-		os.Create("created.txt")
-	})
+	card := widget.NewCard(
+		"VIMRT",
+		"Some info about esport team",
+		widget.NewAccordion(
+			widget.NewAccordionItem(
+				"Teams roster",
+				widget.NewAccordion(
+					widget.NewAccordionItem(
+						"Alexey k0mprend0 Popov",
+						widget.NewLabel("Info about player"),
+					),
+					widget.NewAccordionItem(
+						"Kirill Schaidt Podrez",
+						widget.NewLabel("Info about player"),
+					),
+					widget.NewAccordionItem(
+						"Artem artemixx Veselov",
+						widget.NewLabel("Info about player"),
+					),
+				),
+			),
+			widget.NewAccordionItem(
+				"Teams achivements",
+				widget.NewAccordion(
+					widget.NewAccordionItem(
+						"Faceit legue season 5",
+						widget.NewLabel("Info about tournament"),
+					),
+					widget.NewAccordionItem(
+						"Stealseries i-legue",
+						widget.NewLabel("Info about tournament"),
+					),
+					widget.NewAccordionItem(
+						"Blast Moscow",
+						widget.NewLabel("Info about tournament"),
+					),
+				),
+			),
 
-	file_item2 := fyne.NewMenuItem("Save", func() {
-		fmt.Println("File saved")
-	})
-
-	menu1 := fyne.NewMenu("File", file_item1, file_item2)
-
-	actions_item1 := fyne.NewMenuItem("Hello", func() { fmt.Println("Hello!") })
-	actions_item2 := fyne.NewMenuItem("Bye!", func() { fmt.Println("Goodbye!") })
-	actions_item3 := fyne.NewMenuItem("Button", func() { fmt.Println("Clicked!") })
-
-	menu2 := fyne.NewMenu("Actions", actions_item1, actions_item2, actions_item3)
-
-	info_item1 := fyne.NewMenuItem("Info", func() { fmt.Println("Info!") })
-	info_item2 := fyne.NewMenuItem("Clock me!", func() { fmt.Println("Info 'bout ur amazing app!") })
-	info_item3 := fyne.NewMenuItem("Print", func() { fmt.Println("Printed!") })
-	info_item4 := fyne.NewMenuItem("LOL", func() { fmt.Println("LOL!") })
-	info_item5 := fyne.NewMenuItem("Menu item", func() { fmt.Println("Menu item") })
-	info_item6 := fyne.NewMenuItem("xbox sucks", func() { fmt.Println("Thats true!") })
-
-	menu3 := fyne.NewMenu(
-		"Usless",
-		info_item1,
-		info_item2,
-		info_item3,
-		info_item4,
-		info_item5,
-		info_item6,
+			widget.NewAccordionItem(
+				"More info",
+				widget.NewLabel("No info yet"),
+			),
+		),
 	)
 
-	//всплывающие при наведении окна меню
-	item1 := fyne.NewMenuItem("Options", nil)
-	item2 := fyne.NewMenuItem("Say", nil)
+	window.SetContent(card)
 
-	item1.ChildMenu = fyne.NewMenu(
-		"",
-		fyne.NewMenuItem("Print", func() {
-			fmt.Println("Printed")
-		}),
-		fyne.NewMenuItem("Save", func() {
-			fmt.Println("Saved")
-		}),
-		fyne.NewMenuItem("Cut", func() {
-			fmt.Println("Cutted")
-		}),
-		fyne.NewMenuItem("Copy", func() {
-			fmt.Println("Copied")
-		}),
-		fyne.NewMenuItem("Delete", func() {
-			fmt.Println("Deleted")
-		}),
-	)
+	/*
+		text := "puhasdfihlgadfhasoufhsiuodhfgladhnfojkladbnfilhadgivojhasgiuoadgfoyusdatfiouahnmfoetriunvdgoiygdioyfgvnoidyfgnvioydghnmf8yovtoyiufegnv8ywg 97we9f vw97 rt97wew90puqhg97pwegpiu g8owe w9peuy7"
+		text_label := widget.NewLabel(text)
+		text_label.Wrapping = fyne.TextWrapBreak
+		item1 := widget.NewAccordionItem(
+			"Read more info",
+			text_label,
+		)
 
-	item2.ChildMenu = fyne.NewMenu(
-		"",
-		fyne.NewMenuItem("Hi", func() {
-			fmt.Println("Hello")
-		}),
-		fyne.NewMenuItem("Bye", func() {
-			fmt.Println("Goodbye")
-		}),
-		fyne.NewMenuItem("LOL", func() {
-			fmt.Println("Trolled")
-		}),
-	)
+		item2 := widget.NewAccordionItem(
+			"Button",
+			widget.NewButton(
+				"Say Hello!",
+				func() {
+					fmt.Println("Hello!")
+				},
+			),
+		)
 
-	menu4 := fyne.NewMenu("Buttons", item1, item2)
+		item3 := widget.NewAccordionItem(
+			"Chapters",
+			widget.NewAccordion(
+				widget.NewAccordionItem(
+					"Chapter 1",
+					widget.NewLabel("Some text here..."),
+				),
+				widget.NewAccordionItem(
+					"Chapter 2",
+					widget.NewLabel("Some text from chapter 2 here..."),
+				),
+				widget.NewAccordionItem(
+					"Chapter 3",
+					widget.NewLabel("Very interesting text here..."),
+				),
+			),
+		)
 
-	main_menu := fyne.NewMainMenu(menu1, menu2, menu3, menu4)
+		accordion := widget.NewAccordion(item1, item2, item3)
 
-	window.SetMainMenu(main_menu)
+		window.SetContent(accordion)
+	*/
 
 	//window.SetContent(widget.NewLabel("Hello World!"))
 	window.ShowAndRun()
 }
-
