f24e5d7ec886fac692c7923670da32b1ccf801e7
k0mprend0 <156547649+k0mprend0@users.noreply.github.com>
2026-01-09 13:19:09 +0300

    shapes
    Just some shapes and colors

--- test web app/main.go
+++ test web app/main.go
@@ -6,17 +6,45 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	myApp := app.New()
 	window := myApp.NewWindow("My app")
-	window.Resize(fyne.NewSize(900, 600))
+	window.Resize(fyne.NewSize(500, 500))
 
-	color_for := color.NRGBA{R: 136, G: 0, B: 147, A: 255}
-	label := canvas.NewText("Color", color_for)
+	circle1 := canvas.NewCircle(color.NRGBA{R: 28, G: 221, B: 146, A: 255})
+	circle1.StrokeColor = color.NRGBA{255, 0, 0, 255}
+	circle1.StrokeWidth = 3
 
-	window.SetContent(label)
+	rectangle1 := canvas.NewRectangle(color.NRGBA{95, 99, 208, 255})
+	rectangle1.StrokeColor = color.NRGBA{190, 191, 216, 255}
+	rectangle1.StrokeWidth = 3
+
+	line1 := canvas.NewLine(color.NRGBA{195, 0, 0, 255})
+	line1.StrokeWidth = 5
+
+	icon1 := widget.NewIcon(theme.MediaMusicIcon())
+
+	button := widget.NewButton("Press it", func() {
+		rectangle1.Hide()
+
+		circle1.FillColor = color.NRGBA{45, 36, 36, 255}
+		circle1.StrokeWidth = 6
+
+		line1.StrokeWidth = 8
+		line1.StrokeColor = color.NRGBA{44, 24, 52, 255}
+	})
+
+	content1 := container.NewGridWithColumns(2, circle1, rectangle1)
+	content2 := container.NewGridWithColumns(2, line1, icon1)
+
+	content := container.NewGridWithRows(3, content1, content2, button)
+
+	window.SetContent(content)
 
 	//window.SetContent(widget.NewLabel("Hello World!"))
 	window.ShowAndRun()
