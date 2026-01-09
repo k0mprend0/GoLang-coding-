d522a7f2a5aa234a6efaac7fb7348cdffd38a689
k0mprend0 <156547649+k0mprend0@users.noreply.github.com>
2026-01-09 14:39:42 +0300

    Mini notepad

--- test web app/main.go
+++ test web app/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"image/color"
+	"fmt"
+	"os"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
@@ -14,35 +14,28 @@ import (
 func main() {
 	myApp := app.New()
 	window := myApp.NewWindow("My app")
-	window.Resize(fyne.NewSize(500, 500))
+	window.Resize(fyne.NewSize(250, 250))
+	myApp.Settings().SetTheme(theme.DarkTheme())
 
-	circle1 := canvas.NewCircle(color.NRGBA{R: 28, G: 221, B: 146, A: 255})
-	circle1.StrokeColor = color.NRGBA{255, 0, 0, 255}
-	circle1.StrokeWidth = 3
+	label := widget.NewLabel("Mini notepad")
 
-	rectangle1 := canvas.NewRectangle(color.NRGBA{95, 99, 208, 255})
-	rectangle1.StrokeColor = color.NRGBA{190, 191, 216, 255}
-	rectangle1.StrokeWidth = 3
+	entry := widget.NewMultiLineEntry()
+	entry.SetPlaceHolder("Type sth here...")
+	//entry.Wrapping = fyne.TextWrapBreak //адаптивно к размерам окна
+	//entry.Wrapping = fyne.TextWrapOff //размеры приложения адаптируются к длине строки
 
-	line1 := canvas.NewLine(color.NRGBA{195, 0, 0, 255})
-	line1.StrokeWidth = 5
+	button := widget.NewButton("Save", func() {
+		file, err := os.Create("Info.txt")
+		if err != nil {
+			fmt.Println("Error! - ", err)
+			os.Exit(1)
+		}
+		defer file.Close()
 
-	icon1 := widget.NewIcon(theme.MediaMusicIcon())
-
-	button := widget.NewButton("Press it", func() {
-		rectangle1.Hide()
-
-		circle1.FillColor = color.NRGBA{45, 36, 36, 255}
-		circle1.StrokeWidth = 6
-
-		line1.StrokeWidth = 8
-		line1.StrokeColor = color.NRGBA{44, 24, 52, 255}
+		file.WriteString(entry.Text)
 	})
 
-	content1 := container.NewGridWithColumns(2, circle1, rectangle1)
-	content2 := container.NewGridWithColumns(2, line1, icon1)
-
-	content := container.NewGridWithRows(3, content1, content2, button)
+	content := container.NewVBox(label, entry, button)
 
 	window.SetContent(content)
 
