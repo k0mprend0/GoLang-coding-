4c6ca645d8a5d9f1cfbaacb45721bd81fabdc564
k0mprend0 <156547649+k0mprend0@users.noreply.github.com>
2026-01-12 21:45:15 +0300

    dialog windows

--- test web app/main.go
+++ test web app/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
@@ -15,19 +14,83 @@ func main() {
 	window.Resize(fyne.NewSize(400, 400))
 	myApp.Settings().SetTheme(theme.DarkTheme())
 
-	names := []string{"Камень", "Ножницы", "Бумага"}
+	//types of dialog windows (not all, just some of 'em)
+	//1 Information
+	//2 Confirm
+	//3 CustomConfirm
+	//4 Error
+	//5 Custom
 
-	list := widget.NewList(
-		func() int { return len(names) },
-		func() fyne.CanvasObject {
-			return widget.NewButton("Press it", func() { fmt.Println("...") })
-		},
-		func(id widget.ListItemID, obj fyne.CanvasObject) {
-			obj.(*widget.Button).SetText(names[id])
-		},
-	)
+	/*
+		// 1 Information
+		button := widget.NewButton("Click me", func() {
+			dialog.ShowInformation(
+				"Information about programm",
+				"Quis anim nisi in velit veniam ad dolore elit reprehenderit aliqua.",
+				window,
+			)
+		})
+	*/
 
-	window.SetContent(list)
+	/*
+		// 2 Confirm
+		button := widget.NewButton("Click me", func() {
+			dialog.ShowConfirm(
+				"Do you agree that Grisha dumbass?",
+				"This is annonymus pool",
+				func(b bool) {
+					if b {
+						fmt.Println("This is correct!")
+					} else {
+						fmt.Println("Incorrect")
+					}
+				},
+				window,
+			)
+		})
+	*/
+
+	/*
+		// 3 CustomConfirm
+		button := widget.NewButton("Click me", func() {
+			dialog.ShowCustomConfirm(
+				"Grisha daun?",
+				"YES!",
+				"OF COURSE!",
+				widget.NewLabel("Commodo veniam et Lorem voluptate."),
+				func(b bool) {
+					if b {
+						fmt.Println("This is correct!")
+					} else {
+						fmt.Println("PERFECT")
+					}
+				},
+				window,
+			)
+		})
+	*/
+
+	/*
+		// 4 Error
+		button := widget.NewButton("Click me", func() {
+			dialog.ShowError(
+				errors.New("Unknown issue"),
+				window,
+			)
+		})
+	*/
+
+	// 5 Custom
+	button := widget.NewButton("Click me", func() {
+		dialog.ShowCustom(
+			"Information for user",
+			"I have read it",
+			widget.NewLabel("Duis nulla eiusmod ullamco consequat ea."),
+			window,
+		)
+	})
+
+	window.SetContent(button)
 	//window.SetContent(widget.NewLabel("Hello World!"))
 	window.ShowAndRun()
 }
