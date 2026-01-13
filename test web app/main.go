615c6effda4d97d7b0b71586a9dc2a1eb1b6172d
k0mprend0 <156547649+k0mprend0@users.noreply.github.com>
2026-01-13 15:45:05 +0300

    apptabs

--- test web app/main.go
+++ test web app/main.go
@@ -1,96 +1,62 @@
 package main
 
 import (
-	"fmt"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
+
+func warpLabel(text string) *widget.Label {
+	label := widget.NewLabel(text)
+	label.Wrapping = fyne.TextWrapBreak
+	return label
+}
 
 func main() {
 	myApp := app.New()
 	window := myApp.NewWindow("My app")
 	window.Resize(fyne.NewSize(400, 400))
-	//myApp.Settings().SetTheme(theme.DarkTheme())
+	myApp.Settings().SetTheme(theme.DarkTheme())
 
-	/*
-		1 GridLayout
-		2 GridWarpLayout
-		3 CenterLayout
-	*/
+	img, _ := fyne.LoadResourceFromPath("IMG_9368.jpeg")
 
-	/*
-		// 1 GridLayout
-		l1 := widget.NewLabel("Label 1")
-		l2 := widget.NewLabel("Label 2")
-		l3 := widget.NewLabel("Label 3")
-		l4 := widget.NewLabel("Label 4")
-		l5 := widget.NewLabel("Label 5")
-		l6 := widget.NewLabel("Label 6")
-
-		cont := container.New(
-			layout.NewGridLayout(2),
-			l1,
-			l2,
-			l3,
-			l4,
-			l5,
-			l6,
-		)
-	*/
-
-	/*
-		// 2 GridWarpLayout
-		l1 := widget.NewLabel("Label 1")
-		l2 := widget.NewLabel("Label 2")
-		l3 := widget.NewLabel("Label 3")
-		l4 := widget.NewLabel("Label 4")
-		l5 := widget.NewLabel("Label 5")
-		l6 := widget.NewLabel("Label 6")
-
-		cont := container.New(
-			layout.NewGridWrapLayout(fyne.NewSize(100, 100)),
-			l1,
-			l2,
-			l3,
-			l4,
-			l5,
-			l6,
-		)
-	*/
-
-	// 3 CenterLayout
-	//img := canvas.NewImageFromFile("IMG_9368.jpeg")
-	//img.FillMode = canvas.ImageFillOriginal
-	//text := canvas.NewText("TEXT", color.RGBA{39, 255, 0, 255})
-
-	label := widget.NewLabel("Label")
-	entry := widget.NewEntry()
-	button := widget.NewButton("Button", func() { fmt.Println(entry.Text) })
-
-	cont_l := container.New(
-		layout.NewCenterLayout(),
-		label,
+	cont1 := container.NewVBox(
+		widget.NewLabel("Alex"),
+		widget.NewEntry(),
+		widget.NewButton("Click", func() {}),
 	)
 
-	cont_e := container.New(
-		layout.NewCenterLayout(),
-		entry,
+	cont2 := container.NewVBox(
+		widget.NewLabel("Bro"),
+		widget.NewEntry(),
+		widget.NewButton("Click", func() {}),
 	)
 
-	cont_b := container.New(
-		layout.NewCenterLayout(),
-		button,
+	cont3 := container.NewVBox(
+		widget.NewLabel("Kate"),
+		widget.NewEntry(),
+		widget.NewButton("Click", func() {}),
 	)
 
-	window.SetContent(container.NewVBox(
-		cont_l,
-		cont_e,
-		cont_b,
-	))
+	cont4 := container.NewVBox(
+		widget.NewLabel("John"),
+		widget.NewEntry(),
+		widget.NewButton("Click", func() {}),
+	)
+
+	// simple example
+	tabs := container.NewAppTabs(
+		container.NewTabItemWithIcon("Tab 1", img, cont1),
+		container.NewTabItemWithIcon("Tab 2", img, cont2),
+		container.NewTabItemWithIcon("Tab 3", img, cont3),
+		container.NewTabItemWithIcon("Tab 4", img, cont4),
+	)
+
+	tabs.SetTabLocation(container.TabLocationLeading)
+
+	window.SetContent(tabs)
 	//window.SetContent(widget.NewLabel("Hello World!"))
 	window.ShowAndRun() // !!! change to show6 now ShowAndRun
 	//window.SetMaster() // makes current window main
