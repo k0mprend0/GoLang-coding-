556aa7dc77be8de6f508b1dae64b2b3ab4e60a4c
k0mprend0 <156547649+k0mprend0@users.noreply.github.com>
2026-01-14 13:21:51 +0300

    progress bar

--- test web app/main.go
+++ test web app/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
@@ -22,78 +23,135 @@ func main() {
 	window.Resize(fyne.NewSize(400, 400))
 	myApp.Settings().SetTheme(theme.DarkTheme())
 
-	//label := widget.NewLabel("Slider value")
-
-	slider := widget.NewSlider(0.0, 10.0)
-	//slider.Value = 50.0 // set default value
+	//progress := widget.NewProgressBar() // 0.0 - 1.0 by default
+	//progress.Max = 100.0
 
 	/*
 		entry := widget.NewEntry()
 
-		button := widget.NewButton("Set value", func() {
+		button := widget.NewButton("Change value", func() {
 			value, _ := strconv.ParseFloat(entry.Text, 64)
-			slider.Value = value
-			slider.Refresh()
+			progress.SetValue(value)
+			fmt.Println(progress.Value)
 		})
-
-		window.SetContent(container.NewVBox(
-			slider,
-			entry,
-			button,
-		))
 	*/
 
 	/*
-		// OnChange method
-		slider.OnChanged = func(f float64) {
-			label.SetText(fmt.Sprintf("%f", f))
-		}
+		// example
+		title := widget.NewLabel("Almost done!")
+
+		name := widget.NewEntry()
+		name.SetPlaceHolder("Enter your name")
+
+		surname := widget.NewEntry()
+		surname.SetPlaceHolder("Enter your surname")
+
+		phone_number := widget.NewEntry()
+		phone_number.SetPlaceHolder("Enter your phone number")
+
+		nick := widget.NewEntry()
+		nick.SetPlaceHolder("Enter your nick")
+
+		password := widget.NewPasswordEntry()
+		password.SetPlaceHolder("Your password")
+
+		email := widget.NewEntry()
+		email.SetPlaceHolder("Enter your email")
+
+		bio := widget.NewMultiLineEntry()
+		bio.SetPlaceHolder("Enter additional bio")
+
+		tabs := container.NewAppTabs(
+			container.NewTabItem(
+				"Personal Info",
+				container.NewVBox(
+					name,
+					surname,
+					phone_number,
+				),
+			),
+			container.NewTabItem(
+				"Data for sign up",
+				container.NewVBox(
+					nick,
+					password,
+				),
+			),
+			container.NewTabItem(
+				"Additional Info",
+				container.NewVBox(
+					email,
+					bio,
+				),
+			),
+		)
+
+		next := widget.NewButton("Next step", func() {
+			if tabs.SelectedIndex() < 2 {
+				tabs.SelectIndex(tabs.SelectedIndex() + 1)
+				progress.SetValue(progress.Value + 33)
+			} else {
+				progress.SetValue(progress.Value + 34)
+
+				dialog.ShowInformation(
+					"Sign up completed",
+					"You have been signed up!",
+					window,
+				)
+			}
+		})
 
 		window.SetContent(container.NewVBox(
-			label,
-			slider,
+			title,
+			progress,
+			tabs,
+			next,
 		))
 	*/
 
-	title := widget.NewLabel("Evaluate the app's perfomance from 0 to 10")
-	label := widget.NewLabel("Tell about your experience")
+	// progressbar infinite
+	pbinfinite := widget.NewProgressBarInfinite()
+	pbinfinite.Start()
 
-	feed := widget.NewLabel("Your score wiil be displayed here")
+	/*
+		button := widget.NewButton("Click me", func() {
+			if pbinfinite.Running() {
+				pbinfinite.Stop()
+			} else {
+				pbinfinite.Start()
+			}
+		})
+	*/
 
-	entry := widget.NewEntry()
-	entry.PlaceHolder = "Enter your feedback"
+	// example
+	pbinfinite.Hide()
 
-	button := widget.NewButton("Send feedback", func() { fmt.Println(entry.Text) })
+	title := widget.NewLabel("Create your post")
+	post_title := widget.NewEntry()
+	post_title.SetPlaceHolder("your post title")
 
-	label.Hide()
-	entry.Hide()
-	button.Hide()
+	post_text := widget.NewMultiLineEntry()
+	post_text.SetPlaceHolder("Your text")
 
-	slider.OnChanged = func(f float64) {
-		feed.SetText("Your score: " + fmt.Sprintf("%f", f))
+	submit := widget.NewButton("Submit", func() {
+		pbinfinite.Show()
+		time.Sleep(time.Second * 3)
+		pbinfinite.Hide()
 
-		if f < 5 {
-			label.Show()
-			label.SetText("Tell about your experience")
-			entry.Show()
-			button.Show()
-		} else {
-			label.SetText("Thanks for your feedback")
-			entry.Hide()
-			button.Hide()
-		}
-	}
+		dialog.ShowInformation(
+			"Post creation",
+			"Post have been created",
+			window,
+		)
+	})
 
 	window.SetContent(container.NewVBox(
+		pbinfinite,
 		title,
-		slider,
-		feed,
-		label,
-		entry,
-		button,
+		post_title,
+		post_text,
+		submit,
 	))
-
 	//window.SetContent(widget.NewLabel("Hello World!"))
 	window.ShowAndRun() // !!! change to show6 now ShowAndRun
-	//window.SetMaster() // makes current window main
 }
