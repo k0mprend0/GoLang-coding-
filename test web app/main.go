c2bc3df9279c24c0c6d0dbff77f3a2a22338cadf
k0mprend0 <156547649+k0mprend0@users.noreply.github.com>
2026-01-15 12:20:01 +0300

    dialog window color picker

--- test web app/main.go
+++ test web app/main.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"time"
+	"image/color"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
-
-func warpLabel(text string) *widget.Label {
-	label := widget.NewLabel(text)
-	label.Wrapping = fyne.TextWrapBreak
-	return label
-}
 
 func main() {
 	myApp := app.New()
@@ -23,135 +18,36 @@ func main() {
 	window.Resize(fyne.NewSize(400, 400))
 	myApp.Settings().SetTheme(theme.DarkTheme())
 
-	//progress := widget.NewProgressBar() // 0.0 - 1.0 by default
-	//progress.Max = 100.0
+	text := canvas.NewText("Text to color...", color.White)
 
-	/*
-		entry := widget.NewEntry()
+	rec := canvas.NewRectangle(color.White)
+	rec.SetMinSize(fyne.NewSize(300, 300))
 
-		button := widget.NewButton("Change value", func() {
-			value, _ := strconv.ParseFloat(entry.Text, 64)
-			progress.SetValue(value)
-			fmt.Println(progress.Value)
-		})
-	*/
+	// dialog window ColorPicker
+	cpt := dialog.NewColorPicker(
+		"Color picker",
+		"Choose your own color",
+		func(c color.Color) {
+			text.Color = c
+			text.Refresh()
 
-	/*
-		// example
-		title := widget.NewLabel("Almost done!")
+			rec.FillColor = c
+			rec.Refresh()
+		},
+		window,
+	)
 
-		name := widget.NewEntry()
-		name.SetPlaceHolder("Enter your name")
-
-		surname := widget.NewEntry()
-		surname.SetPlaceHolder("Enter your surname")
-
-		phone_number := widget.NewEntry()
-		phone_number.SetPlaceHolder("Enter your phone number")
-
-		nick := widget.NewEntry()
-		nick.SetPlaceHolder("Enter your nick")
-
-		password := widget.NewPasswordEntry()
-		password.SetPlaceHolder("Your password")
-
-		email := widget.NewEntry()
-		email.SetPlaceHolder("Enter your email")
-
-		bio := widget.NewMultiLineEntry()
-		bio.SetPlaceHolder("Enter additional bio")
-
-		tabs := container.NewAppTabs(
-			container.NewTabItem(
-				"Personal Info",
-				container.NewVBox(
-					name,
-					surname,
-					phone_number,
-				),
-			),
-			container.NewTabItem(
-				"Data for sign up",
-				container.NewVBox(
-					nick,
-					password,
-				),
-			),
-			container.NewTabItem(
-				"Additional Info",
-				container.NewVBox(
-					email,
-					bio,
-				),
-			),
-		)
-
-		next := widget.NewButton("Next step", func() {
-			if tabs.SelectedIndex() < 2 {
-				tabs.SelectIndex(tabs.SelectedIndex() + 1)
-				progress.SetValue(progress.Value + 33)
-			} else {
-				progress.SetValue(progress.Value + 34)
-
-				dialog.ShowInformation(
-					"Sign up completed",
-					"You have been signed up!",
-					window,
-				)
-			}
-		})
-
-		window.SetContent(container.NewVBox(
-			title,
-			progress,
-			tabs,
-			next,
-		))
-	*/
-
-	// progressbar infinite
-	pbinfinite := widget.NewProgressBarInfinite()
-	pbinfinite.Start()
-
-	/*
-		button := widget.NewButton("Click me", func() {
-			if pbinfinite.Running() {
-				pbinfinite.Stop()
-			} else {
-				pbinfinite.Start()
-			}
-		})
-	*/
-
-	// example
-	pbinfinite.Hide()
-
-	title := widget.NewLabel("Create your post")
-	post_title := widget.NewEntry()
-	post_title.SetPlaceHolder("your post title")
-
-	post_text := widget.NewMultiLineEntry()
-	post_text.SetPlaceHolder("Your text")
-
-	submit := widget.NewButton("Submit", func() {
-		pbinfinite.Show()
-		time.Sleep(time.Second * 3)
-		pbinfinite.Hide()
-
-		dialog.ShowInformation(
-			"Post creation",
-			"Post have been created",
-			window,
-		)
+	button := widget.NewButton("Color picker", func() {
+		cpt.Show()
 	})
 
-	window.SetContent(container.NewVBox(
-		pbinfinite,
-		title,
-		post_title,
-		post_text,
-		submit,
-	))
+	window.SetContent(
+		container.NewVBox(
+			button,
+			text,
+			rec,
+		),
+	)
 	//window.SetContent(widget.NewLabel("Hello World!"))
 	window.ShowAndRun() // !!! change to show6 now ShowAndRun
 }
