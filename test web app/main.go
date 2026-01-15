280a94da1e4f05570106427d5f99fee34703775f
k0mprend0 <156547649+k0mprend0@users.noreply.github.com>
2026-01-15 20:50:11 +0300

    file save dialog window

--- test web app/main.go
+++ test web app/main.go
@@ -18,27 +18,22 @@ func main() {
 	myApp.Settings().SetTheme(theme.DarkTheme())
 
 	entry := widget.NewMultiLineEntry()
-	entry.Resize(fyne.NewSize(600, 300))
-	entry.Move(fyne.NewPos(100, 135))
 
-	button := widget.NewButton("Open file", func() {
-		dialog.ShowFileOpen(
-			func(reader fyne.URIReadCloser, err error) {
-				data, _ := io.ReadAll(reader)
-				entry.SetText(string(data))
+	save_file := widget.NewButton("Save file", func() {
+		dialog.ShowFileSave(
+			func(writer fyne.URIWriteCloser, err error) {
+				io.WriteString(writer, entry.Text)
 			},
 			window,
 		)
 	})
-	button.Resize(fyne.NewSize(150, 75))
-	button.Move(fyne.NewPos(325, 30))
 
-	cont := container.NewWithoutLayout(
-		button,
-		entry,
+	window.SetContent(
+		container.NewVBox(
+			entry,
+			save_file,
+		),
 	)
-
-	window.SetContent(cont)
 	//window.SetContent(widget.NewLabel("Hello World!"))
 	window.ShowAndRun() // !!! change to show6 now ShowAndRun
 }
